71e61ee0f639f41adbc6e2a82d49850d98b8d867
atanaskitov <atanaskitov@gmail.com>
2023-02-22 15:01:02 +0200

    main section in progress

--- src/features/Components/Home/Home.jsx
+++ src/features/Components/Home/Home.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './Home.scss';
 
 const Home = () => {
 	return (
 		<div className='main-page'>
 			<Container>
-				<Row>
-					<Col>
-						<div className='navigation'>
-							<ul>
-								<li>За нас</li>
-								<li>Контакти</li>
-							</ul>
-						</div>
+				<Row className='navigation'>
+					<Col md='4' lg='5'>
+						<p className='about-us'>
+							<span>За нас</span>
+						</p>
+					</Col>
+					<Col md='4' lg='2'>
 						<div className='logo'></div>
-						<div className='title-wrapper'></div>
+					</Col>
+					<Col md='4' lg='5'>
+						<p className='contacts'>
+							<span>Контакти</span>
+						</p>
+					</Col>
+				</Row>
+				<Row className='main-title'>
+					<Col md='8' lg='7' className='title-wrapper'>
+						<h1>
+							Открий нови <br />
+							вълнуващи места с нас!
+						</h1>
 					</Col>
 				</Row>
 			</Container>
